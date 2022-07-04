630917fdef4245697463479018fe5f1832546e57
Shivam <shivamkragarwal@gmail.com>
2022-07-04 21:35:38 +0530

    commit

---
+++ lib/ui/screens/members_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MembersDetails extends StatefulWidget {
+  MembersDetails({Key key}) : super(key: key);
+
+  @override
+  _MembersDetailsState createState() => _MembersDetailsState();
+}
+
+class _MembersDetailsState extends State<MembersDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
