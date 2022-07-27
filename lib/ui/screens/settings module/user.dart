1122494edf7e0ce3817d36ada492a1d2e2041c28
Nimish_98 <dan.8potter@gmail.com>
2022-07-27 14:57:11 +0530

    Fix: Code Refactoring

--- lib/ui/screens/settings module/user.dart
+++ lib/ui/screens/settings module/user.dart
@@ -1,14 +1,14 @@
-import 'dart:ui';
-import 'dart:io';
-import 'package:community_internal/core/services/file.service.dart';
 import 'package:community_internal/ui/screens/community_list.dart';
+import 'package:community_internal/ui/widgets/gender_selection.dart';
+import 'package:community_internal/ui/widgets/profile_field.dart';
+import 'package:community_internal/ui/widgets/profile_field_drop_down.dart';
+import 'package:community_internal/ui/widgets/profile_image.dart';
+import 'package:community_internal/widgets/loading_helper.dart';
 import 'package:flutter/material.dart';
 
 List<TextEditingController> _controller = [
   for (int i = 1; i < 13; i++) TextEditingController()
 ];
-List gender = ["Male", "Female", "Other"];
-late String select;
 
 class UserDetails extends StatefulWidget {
   final String phoneNumber;
@@ -19,280 +19,139 @@ class UserDetails extends StatefulWidget {
 }
 
 class _UserDetailsState extends State<UserDetails> {
-  File? _selectedImage;
-  String _selectedGender = 'male';
-  List<Icon> icons = const [
-    Icon(
-      Icons.person,
-      color: Colors.black,
-    ),
-    Icon(
-      Icons.person,
-      color: Colors.black,
-    ),
-    Icon(
-      Icons.phone_android,
-      color: Colors.black,
-    ),
-    Icon(
-      Icons.attach_email,
-      color: Colors.black,
-    ),
-    Icon(
-      Icons.countertops,
-      color: Colors.black,
-    ),
-    Icon(
-      Icons.map_outlined,
-      color: Colors.black,
-    ),
-    Icon(
-      Icons.location_on_sharp,
-      color: Colors.black,
-    ),
-    Icon(
-      Icons.book_outlined,
-      color: Colors.black,
-    ),
-    Icon(Icons.vertical_split, color: Colors.black),
-    Icon(Icons.add_card_outlined, color: Colors.black),
-    Icon(Icons.airplanemode_active, color: Colors.black),
-  ];
-  List<String> data = [
-    "",
-    "Name",
-    "Phone Number",
-    "Email",
-    "State",
-    "District",
-    "Town",
-    "Address",
-    "Religion",
-    "Aadhar card number",
-    "Passport Number"
-  ];
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _aadhaarCardController = TextEditingController();
+  final TextEditingController _passPortController = TextEditingController();
+  bool _isLoading = false;
+  @override
+  void initState() {
+    super.initState();
+    _phoneController.text = widget.phoneNumber;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("User Details"),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: 11,
-              itemBuilder: (context, index) {
-                if (index == 2) {
-                  _controller[index].text = widget.phoneNumber;
-                }
-                return index == 0
-                    ? Stack(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(12),
-                            child: Align(
-                              alignment: Alignment.topLeft,
-                              child: CircleAvatar(
-                                radius: 70,
-                                backgroundColor: Colors.black,
-                                child: CircleAvatar(
-                                  backgroundColor: Colors.white,
-                                  radius: 67,
-                                  backgroundImage:
-                                      _selectedImage?.path.isNotEmpty ?? false
-                                          ? Image.file(_selectedImage!).image
-                                          : const NetworkImage(
-                                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzKgGevOlmVCQ_ROMb5GhVGn-8bCG2ncUNA&usqp=CAU",
-                                            ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 85, horizontal: 130),
-                            child: InkWell(
-                              onTap: () {
-                                final FilePickerService _filePickerService =
-                                    FilePickerService();
-                                final pickedImage =
-                                    _filePickerService.pickImageFromGallery();
-                                pickedImage.then((value) {
-                                  if (value?.path.isNotEmpty ?? false) {
-                                    setState(() {
-                                      _selectedImage = value;
-                                    });
-                                  }
-                                });
-                              },
-                              child: const CircleAvatar(
-                                  radius: 15,
-                                  backgroundColor: Colors.black,
-                                  child: Icon(
-                                    Icons.edit,
-                                    size: 20,
-                                    color: Colors.white,
-                                  )),
-                            ),
-                          ),
-                        ],
-                      )
-                    : index != 4
-                        ? Row(
-                            children: [
-                              Container(
-                                  height: 70,
-                                  width: 30,
-                                  decoration: const BoxDecoration(
-                                    color: Colors.amber,
-                                    borderRadius: BorderRadius.horizontal(
-                                        left: Radius.circular(3)),
-                                  ),
-                                  child: icons[index]),
-                              const SizedBox(
-                                width: 10,
-                              ),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      data[index].toUpperCase(),
-                                      style: const TextStyle(
-                                          fontWeight: FontWeight.w400),
-                                    ),
-                                    TextField(
-                                      controller: _controller[index],
-                                      keyboardType: index == 2
-                                          ? TextInputType.number
-                                          : TextInputType.text,
-                                    ),
-                                  ],
-                                ),
-                              )
-                            ],
-                          )
-                        : Column(
-                            children: [
-                              SizedBox(
-                                height: 80,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Padding(
-                                      padding: EdgeInsets.symmetric(
-                                          vertical: 3, horizontal: 37),
-                                      child: Text(' GENDER:'),
-                                    ),
-                                    Row(
-                                      children: [
-                                        const SizedBox(
-                                          width: 24,
-                                        ),
-                                        Radio<String>(
-                                          value: 'male',
-                                          groupValue: _selectedGender,
-                                          onChanged: (value) {
-                                            setState(() {
-                                              _selectedGender = value!;
-                                            });
-                                          },
-                                        ),
-                                        const Text("MALE"),
-                                        const SizedBox(
-                                          width: 70,
-                                        ),
-                                        Radio<String>(
-                                          value: 'female',
-                                          groupValue: _selectedGender,
-                                          onChanged: (value) {
-                                            setState(() {
-                                              _selectedGender = value!;
-                                            });
-                                          },
-                                        ),
-                                        const Text('FEMALE'),
-                                      ],
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Row(
-                                children: [
-                                  Container(
-                                    height: 70,
-                                    width: 30,
-                                    decoration: const BoxDecoration(
-                                      color: Colors.amber,
-                                      borderRadius: BorderRadius.horizontal(
-                                        left: Radius.circular(3),
-                                      ),
-                                    ),
-                                    child: icons[index],
-                                  ),
-                                  const SizedBox(
-                                    width: 10,
-                                  ),
-                                  Expanded(
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          data[index].toUpperCase(),
-                                          style: const TextStyle(
-                                            fontWeight: FontWeight.w400,
-                                          ),
-                                        ),
-                                        TextField(
-                                          controller: _controller[index],
-                                          keyboardType: index == 2
-                                              ? TextInputType.number
-                                              : TextInputType.text,
-                                        ),
-                                      ],
-                                    ),
-                                  )
-                                ],
-                              )
-                            ],
-                          );
-              },
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.all(25),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(double.maxFinite, 60),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
+    return LoadingHelper(
+      isLoading: _isLoading,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("User Details"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const ProfileImage(),
+              ProfileFeild(
+                icon: const Icon(
+                  Icons.person,
+                  color: Colors.black,
                 ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const CommunityList()),
-                  );
-                },
-                child: const Text(
-                  "NEXT",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 19,
-                  ),
+                displaytText: 'name'.toUpperCase(),
+                controller: _nameController,
+                keyboardType: TextInputType.name,
+              ),
+              ProfileFeild(
+                icon: const Icon(
+                  Icons.phone_android,
+                  color: Colors.black,
+                ),
+                displaytText: 'phone number'.toUpperCase(),
+                controller: _phoneController,
+                keyboardType: TextInputType.phone,
+              ),
+              ProfileFeild(
+                icon: const Icon(
+                  Icons.attach_email,
+                  color: Colors.black,
+                ),
+                displaytText: 'email'.toUpperCase(),
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+              ),
+              const GenderSelection(),
+              const ProfileFieldDropDown(
+                icon: Icon(
+                  Icons.countertops,
+                  color: Colors.black,
                 ),
               ),
-            ),
-          )
-        ],
+              const ProfileFieldDropDown(
+                icon: Icon(
+                  Icons.map_outlined,
+                  color: Colors.black,
+                ),
+              ),
+              const ProfileFieldDropDown(
+                icon: Icon(
+                  Icons.location_on_sharp,
+                  color: Colors.black,
+                ),
+              ),
+              const ProfileFieldDropDown(
+                icon: Icon(
+                  Icons.book_outlined,
+                  color: Colors.black,
+                ),
+              ),
+              const ProfileFieldDropDown(
+                icon: Icon(
+                  Icons.vertical_split,
+                  color: Colors.black,
+                ),
+              ),
+              ProfileFeild(
+                icon: const Icon(
+                  Icons.add_card_outlined,
+                  color: Colors.black,
+                ),
+                displaytText: 'aadhaar card number'.toUpperCase(),
+                controller: _aadhaarCardController,
+                keyboardType: TextInputType.number,
+              ),
+              ProfileFeild(
+                icon: const Icon(
+                  Icons.airplanemode_active,
+                  color: Colors.black,
+                ),
+                displaytText: 'passport number'.toUpperCase(),
+                controller: _passPortController,
+                keyboardType: TextInputType.text,
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.maxFinite, 60),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const CommunityList()),
+                      );
+                    },
+                    child: const Text(
+                      "NEXT",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 19,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
