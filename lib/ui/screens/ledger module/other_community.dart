f655abcd070c54ca4d7cf8fcae1c0748abc4e920
Nimish_98 <dan.8potter@gmail.com>
2022-08-03 00:41:28 +0530

    Fix: UI refactoring other community

--- lib/ui/screens/ledger module/other_community.dart
+++ lib/ui/screens/ledger module/other_community.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 // import '../widgets/dummy_drawer.dart';
 
 List<TextEditingController> _controller = [
-  for (int i = 0; i < 6; i++) TextEditingController()
+  for (int i = 0; i < 5; i++) TextEditingController()
 ];
 late String select;
 
@@ -17,13 +17,7 @@ class OtherCom extends StatefulWidget {
 }
 
 class _OtherComState extends State<OtherCom> {
-  String dropdownvalue = 'First Community';
-  var items = ['First Community', 'Second', 'Third', 'Fourth'];
   List<Icon> icons = const [
-    Icon(
-      Icons.person,
-      color: Colors.black,
-    ),
     Icon(
       Icons.person,
       color: Colors.black,
@@ -46,7 +40,6 @@ class _OtherComState extends State<OtherCom> {
     ),
   ];
   List<String> datas = [
-    "",
     "Doner's Name",
     "Phone number",
     "Address",
@@ -65,105 +58,65 @@ class _OtherComState extends State<OtherCom> {
         children: [
           Expanded(
               child: ListView.builder(
-                  itemCount: 6,
+                  itemCount: 5,
                   itemBuilder: (context, index) {
-                    return index == 0
-                        ? Padding(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 6, horizontal: 4.6),
-                            child: Container(
-                              height: 80,
-                              width: 330,
-                              child: DropdownButton(
-                                value: dropdownvalue,
-                                icon: Align(
-                                    alignment: Alignment.centerRight,
-                                    child:
-                                        const Icon(Icons.keyboard_arrow_down)),
-                                items: items.map((String items) {
-                                  return DropdownMenuItem(
-                                    value: items,
-                                    child: Row(
-                                      children: [
-                                        Text(
-                                          items,
-                                          style: TextStyle(fontSize: 20),
-                                        ),
-                                        SizedBox(
-                                          width: 220,
-                                        )
-                                      ],
-                                    ),
-                                  );
-                                }).toList(),
-                                onChanged: (String? newValue) {
-                                  setState(() {
-                                    dropdownvalue = newValue!;
-                                  });
-                                },
-                              ),
+                    return Row(
+                      children: [
+                        Container(
+                            height: 70,
+                            width: 30,
+                            decoration: const BoxDecoration(
+                              color: Colors.amber,
+                              borderRadius: BorderRadius.horizontal(
+                                  left: Radius.circular(3)),
                             ),
-                          )
-                        : Row(
+                            child: icons[index]),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Container(
-                                  height: 70,
-                                  width: 30,
-                                  decoration: const BoxDecoration(
-                                    color: Colors.amber,
-                                    borderRadius: BorderRadius.horizontal(
-                                        left: Radius.circular(3)),
-                                  ),
-                                  child: icons[index]),
-                              SizedBox(
-                                width: 10,
+                              Text(
+                                datas[index].toUpperCase(),
+                                style: TextStyle(fontWeight: FontWeight.w400),
                               ),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      datas[index].toUpperCase(),
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w400),
-                                    ),
-                                    TextField(
-                                      controller: _controller[index],
-                                      keyboardType: index == 2
-                                          ? TextInputType.number
-                                          : TextInputType.text,
-                                    ),
-                                  ],
-                                ),
-                              )
+                              TextField(
+                                controller: _controller[index],
+                                keyboardType: index == 2
+                                    ? TextInputType.number
+                                    : TextInputType.text,
+                              ),
                             ],
-                          );
+                          ),
+                        )
+                      ],
+                    );
                   })),
           Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
               padding: const EdgeInsets.all(25),
               child: ElevatedButton(
-                style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25),
-                ))),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.maxFinite, 60),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                ),
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => CommunityList()),
                   );
                 },
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 15, horizontal: 120),
-                  child: Text(
-                    "NEXT",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 19),
+                child: const Text(
+                  "SAVE",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 19,
                   ),
                 ),
               ),
