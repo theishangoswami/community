053978e375c7bd167cdf8a95b9cb86a3e38c7edf
Nimish_98 <dan.8potter@gmail.com>
2022-08-18 20:13:04 +0530

    Fix: Base url

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
   static const String baseUrl =
-      'https://mydemoweb.online/community_app/admin/api/';
+      'https://www.akhilbhartiyasamaj.com/api/';
   static const String imageBaseUrl =
       "http://mydemoweb.online/community_app/admin/api/src/routes/upload/";
 }
