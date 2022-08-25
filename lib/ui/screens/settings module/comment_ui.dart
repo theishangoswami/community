bd7d070a817f242dda8c5e76595f33804c946d87
Nimish_98 <dan.8potter@gmail.com>
2022-08-25 23:22:17 +0530

    Fix: Fixed comment page

--- lib/ui/screens/settings module/comment_ui.dart
+++ lib/ui/screens/settings module/comment_ui.dart
@@ -1,94 +1,105 @@
 import 'package:flutter/material.dart';
 
-class Commentt extends StatefulWidget {
-  const Commentt({Key? key}) : super(key: key);
+class CommentPage extends StatefulWidget {
+  const CommentPage({Key? key}) : super(key: key);
 
   @override
-  State<Commentt> createState() => _CommenttState();
+  State<CommentPage> createState() => _CommentPageState();
 }
 
-class _CommenttState extends State<Commentt> {
-  List<String> user=["awesome","nice","cool"];
-  TextEditingController data=new TextEditingController();
-  var comment="Awesome";
+class _CommentPageState extends State<CommentPage> {
+  List<String> user = ["awesome", "nice", "cool"];
+  TextEditingController data = TextEditingController();
+  var comment = "Awesome";
   void clearText() {
     data.clear();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Comments".toUpperCase()),
-        actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.send))
-        ],
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.send))],
       ),
       body: Stack(
         alignment: Alignment.bottomCenter,
         children: [
-          Container(
+          SizedBox(
             height: 800,
-            child: ListView.builder(itemBuilder: (context,index){
-              return
-                Column(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Column(
                   children: [
-                    index==0?SizedBox(
-                      height: 10,
-                    ):Container(),
+                    index == 0
+                        ? const SizedBox(
+                            height: 10,
+                          )
+                        : Container(),
                     ListTile(
-                      leading:CircleAvatar(
-                        child:Image.asset("assets/images/friend.png"),
+                      leading: CircleAvatar(
+                        child: Image.asset("assets/images/friend.png"),
                       ),
                       title: Row(
                         children: [
-                          Text("Nihal".toUpperCase(),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17),),
-                          SizedBox(
+                          Text(
+                            "Nihal".toUpperCase(),
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 17),
+                          ),
+                          const SizedBox(
                             width: 7,
                           ),
-                          Text(user[index],style: TextStyle(fontWeight: FontWeight.w300,fontSize: 14)),
+                          Text(user[index],
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w300, fontSize: 14)),
                         ],
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(3),
+                    const Padding(
+                      padding: EdgeInsets.all(3),
                       child: Divider(
                         color: Colors.black,
                       ),
                     )
                   ],
                 );
-            },itemCount: user.length,),
+              },
+              itemCount: user.length,
+            ),
           ),
           Container(
-            height:80,
+            height: 80,
             color: Colors.white,
             child: Padding(
               padding: const EdgeInsets.symmetric(vertical: 19),
               child: ListTile(
-                leading:CircleAvatar(
-                  child:Image.asset("assets/images/friend.png"),
+                leading: CircleAvatar(
+                  child: Image.asset("assets/images/friend.png"),
                 ),
                 title: Padding(
                   padding: const EdgeInsets.all(5),
-                  child: TextField (
+                  child: TextField(
                     controller: data,
                     decoration: InputDecoration(
                         border: InputBorder.none,
-                        hintText: 'Comment as Nihal...'.toUpperCase()
-                    ),
+                        hintText: 'Comment as Nihal...'.toUpperCase()),
                   ),
                 ),
                 trailing: GestureDetector(
-                    onTap: (){
-                      setState((){
-                        comment=data.text;
+                    onTap: () {
+                      setState(() {
+                        comment = data.text;
                         user.add(comment);
                         clearText();
                       });
                     },
-                    child:Padding(
+                    child: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Text("Post".toUpperCase(),style: TextStyle(fontSize: 15),),
+                      child: Text(
+                        "Post".toUpperCase(),
+                        style: const TextStyle(fontSize: 15),
+                      ),
                     )),
               ),
             ),
